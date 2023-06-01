cfe1bf15235a8c79bc1ba808c9caa84850ae6a05
RajChandan <raj.chandan91@gmail.com>
2023-06-02 02:28:34 +0530

    contact setup

--- Kodera_Site/Kodera_Site/urls.py
+++ Kodera_Site/Kodera_Site/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("Home.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("Home.urls")),
+    path("contact/", include("Contact.urls")),
+]
