fa665b783ce0cbeecc76226f670409469992688e
RajChandan <raj.chandan91@gmail.com>
2023-06-13 00:52:45 +0530

    contact model

--- Kodera_Site/Contact/models.py
+++ Kodera_Site/Contact/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class YearField(models.DateField):
+    def __init__(self, *args, **kwargs):
+        kwargs["validators"] = [
+            MinValueValidator(2010),  # Specify the minimum year
+            MaxValueValidator(2028),  # Specify the maximum year
+        ]
+        super().__init__(*args, **kwargs)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class details(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    graduation_year = YearField()
+    job_title = models.OneToOneField(Job, on_delete=models.CASCADE)
+    contacted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- Kodera_Site/Contact/views.py
+++ Kodera_Site/Contact/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def contact(request):
     print(request.method)
-    return render(request, "Contact/contact.html")
+    year_range = range(2010, 2028)
+    job_titles = ["A", "B"]
+    if request.method == "POST":
+        print(request.POST)
+    return render(
+        request,
+        "Contact/contact.html",
+        {"year_range": year_range, "job_titles": job_titles},
+    )
